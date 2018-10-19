8fa539a661ee7598dbb1b2d23823ec763e05f820
Fabio9791 <fabiofigueiredo9791@gmail.com>
2018-10-19 16:30:15 +0200

    exo 4 php

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,22 @@
 <?php
+/*$date = new DateTime('first monday of  at midnight');
+$lastDate = new DateTime('last day of the current month');
+
+$date->add(new DateInterval('+1day'));
+$dateAsString = $date->format('Y-m-d H:i:s');*/ 
+
+function getAllMondaysOfMonth($year,$month) {
+    $date = DateTime::createFromFormat('Yn', $year.$month);
+    $date = new DateTime('first day of '.$date->format('F Y'));
+    $allmondays=[];
+    while($date->format('m')==$month){
+        if($date->format('N')==1){
+            array_push($allmondays, $date->format('l j, M Y'));
+        }
+        $date->add(DateInterval::createFromDateString('next monday'));
+    }
+    return $allmondays;
+   // var_dump($allmondays);
+}
+
+//getAllMondaysOfMonth(2018, 07);
