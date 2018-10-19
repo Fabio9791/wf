dc6e3677afb312bcc03d707ea6f5a84fbde2d63b
Fabio9791 <fabiofigueiredo9791@gmail.com>
2018-10-19 12:09:12 +0200

    exo 3 php

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,19 @@
 <?php
 
 $input;
-$winner;
+$a=0;
+$b=0;
+$winner="";
+foreach($input as $round){
+    if($round[0]>$round[1]){
+        $a++;
+    } elseif($round[1]>$round[0]){
+        $b++;
+    }
+};
+if($a>$b){
+    $winner="A";
+} else if($a<$b){
+    $winner="B";
+}
+echo $winner;
