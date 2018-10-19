a498dad1d5026ed2a0e8ff95e2330dc1b0997345
Fabio9791 <fabiofigueiredo9791@gmail.com>
2018-10-19 10:27:51 +0200

    exo 2 php

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,5 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$saltedPassword = substr($password,0,ceil(strlen($password)/2)).$salt.substr($password,ceil(strlen($password)/2),floor(strlen($password)/2));
+echo $saltedPassword;
