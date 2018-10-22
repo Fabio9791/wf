210f94dec14bdc007d1b2ff688e8e2ef98da2193
Fabio9791 <fabiofigueiredo9791@gmail.com>
2018-10-22 17:06:55 +0200

    sdfds

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,21 @@
 <?php
+function divide(int $pDivide,int $pDivisor):int{
+    if($pDivisor!=0){
+        return $pDivide/$pDivisor;
+    }else{
+        throw new RuntimeException('Division by 0 is not allowed');
+    }
+}
+
+function arrayDivide(array $pDivide,int $pDivisor):array{
+    $array=[];
+    try{
+        foreach ($pDivide as $current){
+            array_push($array,divide($current, $pDivisor));
+        }
+        return $array;
+    } catch(RuntimeException $e){
+        return $pDivide;
+    }
+    
+}
