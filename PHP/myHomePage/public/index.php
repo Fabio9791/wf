dac7c29633b96329b3ebd039fce6d2c7fcc942df
Fabio9791 <fabiofigueiredo9791@gmail.com>
2018-10-25 17:02:47 +0200

    fdsfGBFD

--- PHP/myHomePage/public/index.php
+++ PHP/myHomePage/public/index.php
@@ -1,4 +1,15 @@
 <?php
 
-include __DIR__ . '/../model/articles.php';
-include __DIR__ . '/../view/articleList.php';
+$config = include __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../model/Project.php';
+
+try{
+    $projects=getAllProjects();
+}catch(Exception $e){
+    echo 'An error occured with code : '.$e->getMessage();
+    exit;
+}
+
+var_dump($projects->fetchAll());
+/*include __DIR__ . '/../model/articles.php';
+include __DIR__ . '/../view/articleList.php';*/
